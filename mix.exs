e6d7e951a2c21e614effd32d87c6f6e1d7713a6b
Shane Howley <shane@grain.co>
2024-04-11 16:13:20 +0100

    Relax ueberauth version requirement

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule UeberauthSalesforce.Mixfile do
   defp deps do
     [
       {:oauth2, "~> 1.0 or ~> 2.0"},
-      {:ueberauth, "~> 0.7.0"},
+      {:ueberauth, "~> 0.7"},
       {:credo, ">= 0.0.0", only: [:dev], runtime: false},
       {:ex_doc, ">= 0.0.0", only: [:dev], runtime: false}
     ]
